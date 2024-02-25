25781b6d159d7fc75531a14c9d3ba24c729657c7
galakpaigates <jeedoarkoi2006@gmail.com>
2024-02-25 00:49:46 +0000

    added alert to show whenever some plays the game for the first time switching to compile and test for windows

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3.11
 
 from tkinter import *
-import random, sys, os
-from pathlib import Path
+import random, sys, os, smtplib, ssl, platform, getpass
+from email.message import EmailMessage
+
+PERSONAL_MAIL_ADDR = "jeedoarkoi@gmail.com"
+MAIL_ADDR = "galakpaigates@gmail.com"
+APP_PASSWORD = "tscg phol rbhl mygg"
 
 GAME_WIDTH = 630
 GAME_HEIGHT = 630
@@ -13,7 +17,7 @@ SNAKE_COLOR = "wheat"
 FOOD_COLOR = "brown"
 BACKGROUND_COLOR = "tan"
 
-# global but !constant
+# global vars but !constant
 canvas = None
 label = None
 window = None
@@ -25,6 +29,13 @@ mode = "first"
 
 # main function begins here
 def main():
+    
+    # alert galakpaigates that someone is playing the game
+    if check_first_run():
+        try:
+            send_email()
+        except Exception:
+            pass
     
     global canvas
     global label
@@ -78,7 +89,8 @@ class Snake:
             self.coordinates.append([0, 0])
 
         for x, y in self.coordinates:
-            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            # Draw the rectangle with a border radius of 15%
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, outline=SNAKE_COLOR, width=2)
             self.squares.append(square)
 
 class Food:
@@ -199,6 +211,56 @@ def replay():
     # rerun the entire program
     py_interpreter = sys.executable
     os.execl(py_interpreter, py_interpreter, *sys.argv)
+    
+
+def send_email():
+    my_email = PERSONAL_MAIL_ADDR
+    
+    os_name = platform.system()
+    username = os.path.abspath(__name__)
+    
+    # use the right method to get the user name for the right OS
+    if os_name == "Linux":
+        username = os.getlogin()
+        
+    elif os_name == "Windows":
+        username = getpass.getuser()
+
+    # create email header and content
+    message = EmailMessage()
+    
+    html = f"""
+        <html>
+            <body>
+                <h2>{username} played the Snake Game on a {os_name} OS</h2>
+            </body>
+        </html>
+    """
+
+    message["To"] = my_email
+    message["From"] = MAIL_ADDR
+    message["Subject"] = "Snake Game Player"
+    
+    message.add_alternative(html, subtype="html")
+
+    # create a ssl context to make sure the mail transfer is secure
+    context = ssl.create_default_context()
+
+    # use gmail server to do the mail transfer
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(user=MAIL_ADDR, password=APP_PASSWORD)
+        server.sendmail(from_addr=MAIL_ADDR, to_addrs=my_email, msg=message.as_string())
+        
+        
+def check_first_run():
+    flag_file = ".youveplayed.flag"
+    if not os.path.exists(flag_file):
+        # First run, create the flag file
+        with open(flag_file, "w") as f:
+            f.write("You've played Galakpai Gates' Snake Game!")
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
